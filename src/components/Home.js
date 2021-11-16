efb191602b51b358410e4321f6d6b03d72218db5
gastonkornfeld <gatok37@gmail.com>
2021-11-16 16:04:41 +0200

    les try

---
+++ src/components/Home.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import styled from "styled-components"
+import Section from './Section'
+
+function Home() {
+    return (
+        <Container>
+            <Section 
+                title="Model-S"
+                description = "Order online for Touchless delivery"
+                backgroundImg ="model-s.jpg"
+                leftbuttxt = "Custom order"
+                rightbuttxt = "Exisiting Inventory"
+            />
+            <Section 
+                title="Model-Y"
+                description = "Order online for Touchless delivery"
+                backgroundImg ="model-y.jpg"
+                leftbuttxt = "Custom order"
+                rightbuttxt = "Exisiting Inventory"
+            />
+            <Section 
+                title="Model-X"
+                description = "Order online for Touchless delivery"
+                backgroundImg ="model-x.jpg"
+                leftbuttxt = "Custom order"
+                rightbuttxt = "Exisiting Inventory"
+            />
+            <Section 
+                title="Model-3"
+                description = "Order online for Touchless delivery"
+                backgroundImg ="model-3.jpg"
+                leftbuttxt = "Custom order"
+                rightbuttxt = "Exisiting Inventory"
+            />
+            <Section 
+                title="Lower cost solar panel in america"
+                description = "Money Back guarantee"
+                backgroundImg ="solar-panel.jpg"
+                leftbuttxt = "Order Now"
+                rightbuttxt = "Learn More"
+            />
+            <Section 
+                title="Solar for new roofs"
+                description = "Solar roof cost less than a  new roof plus solar panels"
+                backgroundImg ="solar-roof.jpg"
+                leftbuttxt = "Order Now"
+                rightbuttxt = "Learn More"
+            />
+            <Section 
+                title="Accesories"
+                description = ""
+                backgroundImg ="accessories.jpg"
+                leftbuttxt = "Shop Now"
+                
+            />
+        </Container>
+    )
+}
+
+
+
+export default Home
+
+const Container = styled.div`
+    height: 100vh;
+`
---
+++ src/components/Section.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import styled from "styled-components"
+import Fade from 'react-reveal/Fade';
+
+function Section({title, description, leftbuttxt, rightbuttxt, backgroundImg}) {
+    return (
+        <Wrap bgImage={backgroundImg}>
+            <Fade bottom>
+                <Itemtext>
+                    <h1> {title}</h1>
+                    <p>{description}</p>
+                </Itemtext>
+            </Fade>
+            <Buttons>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftbuttxt}
+                        </LeftButton>
+                        {rightbuttxt && 
+                            <RightButton>
+                                {rightbuttxt}
+                            </RightButton>
+                        }
+                        
+
+                    </ButtonGroup>
+                </Fade>
+                <DownArrow src="images/down-arrow.svg" />
+            </Buttons>
+        </Wrap>
+    )
+}
+
+export default Section
+
+ 
+const Wrap = styled.div`
+    
+    width: 100vw;
+    height: 100vh;
+    backround-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: ${props => `url("/images/${props.bgImage}")`};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`
+
+
+const Itemtext = styled.div`
+    z-index: 10;    
+    padding-top: 10vh;
+    text-align: center;
+`
+
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+
+`
+
+const LeftButton = styled.div`
+    background-color: rgb(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 20px;
+    cursor: pointer;
+    margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.65;
+    color: black;
+`
+
+const DownArrow = styled.img`
+    
+    height:40px;
+    animation: animateDown infinite 1.5s;
+    overflow-x: hidden;
+
+
+`
+
+const Buttons = styled.div`
+
+`
