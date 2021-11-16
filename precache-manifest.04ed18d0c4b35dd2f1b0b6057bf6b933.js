e85914f57875028290a159755f0d982c8920cb30
gastonkornfeld <gatok37@gmail.com>
2021-11-16 16:06:25 +0200

    Updates

---
+++ precache-manifest.04ed18d0c4b35dd2f1b0b6057bf6b933.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39bf5ab10cc2b9433341f811e88afe3",
+    "url": "/tesla/index.html"
+  },
+  {
+    "revision": "607e1f1705180a0d8246",
+    "url": "/tesla/static/css/main.59271894.chunk.css"
+  },
+  {
+    "revision": "fcdbffb9dd1dacb5f25c",
+    "url": "/tesla/static/js/2.66556c89.chunk.js"
+  },
+  {
+    "revision": "ea88211925d18270e09b4691b32a2900",
+    "url": "/tesla/static/js/2.66556c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607e1f1705180a0d8246",
+    "url": "/tesla/static/js/main.c8cab4f6.chunk.js"
+  },
+  {
+    "revision": "265b4129a80ee8ec3640",
+    "url": "/tesla/static/js/runtime-main.e2090933.js"
+  }
+]);
